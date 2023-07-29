2cbd8e32588f04d56e3e95c7e58f1d53bbfe5a8f
OH CHANGMIN <lou0124@naver.com>
2023-07-29 13:43:29 +0900

    feat: 초기 데이터 세팅

---
+++ src/main/java/com/mutsa/mutsamarket/TestDataInit.java
@@ -0,0 +1,55 @@
+package com.mutsa.mutsamarket;
+
+import com.mutsa.mutsamarket.entity.Item;
+import com.mutsa.mutsamarket.entity.Users;
+import com.mutsa.mutsamarket.repository.ItemRepository;
+import com.mutsa.mutsamarket.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import static com.mutsa.mutsamarket.entity.enumtype.ItemStatus.SALE;
+
+@Profile("dev")
+@Component
+@RequiredArgsConstructor
+public class TestDataInit {
+
+    private final UserRepository userRepository;
+    private final ItemRepository itemRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void initData() {
+        Users kim = Users.builder()
+                .username("kim")
+                .password(passwordEncoder.encode("1234"))
+                .build();
+
+        Users park = Users.builder()
+                .username("park")
+                .password(passwordEncoder.encode("1234"))
+                .build();
+
+        userRepository.save(kim);
+        userRepository.save(park);
+
+        Item item1 = Item.builder().title("item1")
+                .description("description1")
+                .user(park).minPriceWanted(1000)
+                .status(SALE)
+                .build();
+
+        Item item2 = Item.builder().title("item2")
+                .description("description2")
+                .user(park).minPriceWanted(2000)
+                .status(SALE)
+                .build();
+
+        itemRepository.save(item1);
+        itemRepository.save(item2);
+    }
+}
