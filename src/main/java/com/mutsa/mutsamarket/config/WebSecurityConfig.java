21cc20bb877a21491d8cd44767791bf2960174d9
OH CHANGMIN <lou0124@naver.com>
2023-07-29 15:26:06 +0900

    feat: 아이템 상세정보 보기 구현

--- src/main/java/com/mutsa/mutsamarket/config/WebSecurityConfig.java
+++ src/main/java/com/mutsa/mutsamarket/config/WebSecurityConfig.java
@@ -4,13 +4,13 @@ import com.mutsa.mutsamarket.jwt.JwtTokenFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.intercept.AuthorizationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 
 @Configuration
@@ -30,14 +30,13 @@ public class WebSecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
                         authHttp -> authHttp
-                                .requestMatchers(HttpMethod.GET, "/api/items").permitAll()
-                                .requestMatchers(HttpMethod.GET, "/api/items/{itemId}").permitAll()
-                                .requestMatchers(HttpMethod.GET, "/api/items/{itemId}/comments").permitAll()
-                                .requestMatchers(
-                                        "/api/auth/**",
-                                        "/auth/**"
-                                ).permitAll()
-                                .anyRequest().authenticated()
+                                .requestMatchers(new AntPathRequestMatcher("/api/items", "GET")).permitAll()
+                                .requestMatchers(new AntPathRequestMatcher("/api/items/{itemId}", "GET")).permitAll()
+                                .requestMatchers(new AntPathRequestMatcher("/api/items/{itemId}/comments", "GET")).permitAll()
+                                .requestMatchers(new AntPathRequestMatcher("/api/auth/**")).permitAll()
+                                .requestMatchers(new AntPathRequestMatcher("/auth/**")).permitAll()
+                                .requestMatchers(new AntPathRequestMatcher("/items", "GET")).permitAll()
+                                .requestMatchers(new AntPathRequestMatcher("/items/{itemId}", "GET")).permitAll()
                 )
                 .formLogin(
                         formLogin -> formLogin
--- src/main/java/com/mutsa/mutsamarket/controller/controller/ItemController.java
+++ src/main/java/com/mutsa/mutsamarket/controller/controller/ItemController.java
@@ -1,12 +1,14 @@
 package com.mutsa.mutsamarket.controller.controller;
 
 import com.mutsa.mutsamarket.controller.response.ItemResponse;
+import com.mutsa.mutsamarket.entity.Item;
 import com.mutsa.mutsamarket.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,11 +19,20 @@ public class ItemController {
     private final ItemService itemService;
 
     @GetMapping
-    public String home(Model model) {
+    public String itemList(Model model) {
         Page<ItemResponse> items = itemService.findItems(1, 25)
                 .map(ItemResponse::fromEntity);
 
         model.addAttribute("items", items);
         return "items/list";
+    }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable Long itemId, Model model) {
+        Item findItem = itemService.findItem(itemId);
+
+        ItemResponse item = ItemResponse.fromEntity(findItem);
+        model.addAttribute("item", item);
+        return "items/item";
     }
 }
